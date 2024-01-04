4cf29db04ccb55957453ea22f4b94b6b75775fb9
Elian Gerard <eliangerardxd@gmail.com>
2024-01-03 21:25:08 -0600

    first commit

---
+++ src/content/stats.js
@@ -0,0 +1,115 @@
+export const stats = {
+    "categorias": [
+      {
+        "categoria": "mensajes",
+        "usuarios": [
+          {"usuario": "Servidor", "puntaje": 11194},
+          {"avatar": "https://images-ext-2.discordapp.net/external/y2vCUj5rDYILCnvLSSJzFO87Gg1IPf1v4YsfI6LBTkc/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/915662521388654642/1d590c39ecb6378f7302686143425c24.webp?format=webp&width=671&height=671","usuario": "meshogang",  "puntaje": 1503},
+          {"avatar": "https://images-ext-2.discordapp.net/external/NJk33XtuuD3k_0Ci5_KTwvmoCRGZ7gQsRsc-qKQqS6M/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/743562599307804682/750b55291831f46102c5b8ba56f6393c.webp?format=webp","usuario": "renacido",  "puntaje": 396},
+          {"avatar": "https://images-ext-2.discordapp.net/external/Ba31NLqZSY9VCuHirk76L0uP2WfTvxGb2_S-kWUOgj0/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/456635117327089665/30bcb324fbdc30209e86ebf0cad15cae.webp?format=webp","usuario": "Isra", "puntaje": 100},
+          {"avatar": "https://images-ext-1.discordapp.net/external/qca5zJ3u_ZmIOARmSplzgCuL61UpnlV2jJgVDspUyF4/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/352549144918753281/6b5119eb2a83feb37848a0c47c24f791.webp?format=webp","usuario": "subaru legacy", "puntaje": 1798},
+          {"avatar": "https://images-ext-2.discordapp.net/external/sXhKc3uOLl247DaBjfXKrUisgI6ROZug7eZRt_8Lo3U/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/300788227768320000/fd5f1bc71f9007db77c12173eeec03ca.webp?format=webp","usuario": "caballerito", "puntaje": 615},
+          {"avatar": "https://images-ext-2.discordapp.net/external/Y-XNka8iHk5I6uSkg6fMtOaBQtET-rpNCiAoweojZP0/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/284084418698805249/573098231f98bc325d88e691fde5def8.webp?format=webp&width=671&height=671","usuario": "RoyManky", "puntaje": 846},
+          {"avatar": "https://images-ext-2.discordapp.net/external/gOI22kY75wRD4qUd9tnH2xH1slGWrNcAJPlxFAl2wzI/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/430143146886692875/7b0ff78854c5d7b817700225a9e1b81f.webp?format=webp&width=671&height=671","usuario": "andale elian vamos a tatuarnos", "puntaje": 339},
+          {"avatar": "https://images-ext-1.discordapp.net/external/mB2i5MDOsz6JLCsq1AAjP5TaGRePoeDs-c_LLTsIh3Q/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/837165383697956964/1e42539284a56842817b61f059f2d79a.webp?format=webp&width=671&height=671","usuario": "milanwiche", "puntaje": 809},
+          {"avatar": "https://images-ext-2.discordapp.net/external/k6UvB840D8sVBDxKajkszby_NC7vmAJ9HPuqDEvzOyw/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/664341024113426442/5e890b6b4d1dda2c61d6af98a01e43c8.webp?format=webp","usuario": "Albyst", "puntaje": 190},
+          {"avatar": "https://images-ext-1.discordapp.net/external/7beiUzryiRYEnaSVCMzJDTmMhXSA_mBa3R4Q1neexZ4/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/366055205717934092/21b7f5bff63ff5da8235cda1bb1ac4c4.webp?format=webp","usuario": "Kurco", "puntaje": 11},
+          {"avatar": "https://images-ext-2.discordapp.net/external/JthZIYXO3eRkcoFmsAy8CCmDopFJABP8fBqBt1SQ4y8/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/522112771438018570/0a12ac383e36e81b9bbe997c0ee94203.webp?format=webp","usuario": "tilin", "puntaje": 237},
+          {"avatar": "https://images-ext-2.discordapp.net/external/6_CNZVbmcGPSaOYcXG69fVeamfdJOgxsHoaqcMwJalk/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/246126137070059520/39e74ec85f4466a4d185f5038cc91951.webp?format=webp&width=671&height=671","usuario": "Poyotota", "puntaje": 473},
+          {"avatar": "https://images-ext-2.discordapp.net/external/dhXRyr5XNusjHp9r2ZTzKPFl6qt1hXoHQCLrMZEA3no/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/276193332764606464/8116bd1e1d7c642163191f428c4d4243.webp?format=webp&width=671&height=671","usuario": "chavez smith", "puntaje": 802},
+          {"avatar": "https://images-ext-2.discordapp.net/external/JF2IpJ6dpWLEkhWihK1oZZyvuBco9-XFcsoO4WSMQC4/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/459167039542657025/1839188f8ae5d45d20e57d85580fe3dd.webp?format=webp&width=671&height=671","usuario": "el idolo", "puntaje": 411},
+          {"avatar": "https://images-ext-1.discordapp.net/external/TgFsAXgCjQiZPYPM64CQnavaFfYu4CmORTbRo-gqqg0/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/913592895674646529/878b29845a061818b556644a9a1721ba.webp?format=webp","usuario": "ch4", "puntaje": 388},
+          {"avatar": "https://images-ext-2.discordapp.net/external/bK1mTcYfDuAp57ZQw9Xi6wAG4SwuNuKCvbkYh7LuimI/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/407665592846385152/d256840d191895e33456b27fed35b04b.webp?format=webp","usuario": "black eskater", "puntaje": 70}
+        ]
+      },
+      {
+        "categoria": "imagenes",
+        "usuarios": [
+          {"usuario": "Servidor", "puntaje": 2386},
+          {"usuario": "meshogang", "puntaje": 389},
+          {"usuario": "renacido", "puntaje": 50},
+          {"usuario": "Isra", "puntaje": 23},
+          {"usuario": "subaru legacy", "puntaje": 723},
+          {"usuario": "caballerito", "puntaje": 72},
+          {"usuario": "RoyManky", "puntaje": 189},
+          {"usuario": "andale elian vamos a tatuarnos", "puntaje": 109},
+          {"usuario": "milanwiche", "puntaje": 114},
+          {"usuario": "Albyst", "puntaje": 13},
+          {"usuario": "Kurco", "puntaje": 0},
+          {"usuario": "tilin", "puntaje": 9},
+          {"usuario": "Poyotota", "puntaje": 145},
+          {"usuario": "chavez smith", "puntaje": 154},
+          {"usuario": "el idolo", "puntaje": 45},
+          {"usuario": "ch4", "puntaje": 39},
+          {"usuario": "black eskater", "puntaje": 9}
+        ]
+      },
+      {
+        "categoria": "links",
+        "usuarios": [
+          {"usuario": "Servidor", "puntaje": 1403},
+          {"usuario": "meshogang", "puntaje": 226},
+          {"usuario": "renacido", "puntaje": 11},
+          {"usuario": "Isra", "puntaje": 12},
+          {"usuario": "subaru legacy", "puntaje": 361},
+          {"usuario": "caballerito", "puntaje": 49},
+          {"usuario": "RoyManky", "puntaje": 141},
+          {"usuario": "andale elian vamos a tatuarnos", "puntaje": 41},
+          {"usuario": "milanwiche", "puntaje": 79},
+          {"usuario": "Albyst", "puntaje": 16},
+          {"usuario": "Kurco", "puntaje": null},
+          {"usuario": "tilin", "puntaje": 5},
+          {"usuario": "Poyotota", "puntaje": 32},
+          {"usuario": "chavez smith", "puntaje": 170},
+          {"usuario": "el idolo", "puntaje": 9},
+          {"usuario": "ch4", "puntaje": 67},
+          {"usuario": "black eskater", "puntaje": 3}
+        ]
+      },
+      {
+        "categoria": "videos",
+        "usuarios": [
+          {"usuario": "Servidor", "puntaje": 276},
+          {"usuario": "meshogang", "puntaje": 31},
+          {"usuario": "renacido", "puntaje": 2},
+          {"usuario": "Isra", "puntaje": 0},
+          {"usuario": "subaru legacy", "puntaje": 107},
+          {"usuario": "caballerito", "puntaje": 28},
+          {"usuario": "RoyManky", "puntaje": 16},
+          {"usuario": "andale elian vamos a tatuarnos", "puntaje": 2},
+          {"usuario": "milanwiche", "puntaje": 28},
+          {"usuario": "Albyst", "puntaje": null},
+          {"usuario": "Kurco", "puntaje": null},
+          {"usuario": "tilin", "puntaje": 5},
+          {"usuario": "Poyotota", "puntaje": 19},
+          {"usuario": "chavez smith", "puntaje": 11},
+          {"usuario": "el idolo", "puntaje": null},
+          {"usuario": "ch4", "puntaje": 7},
+          {"usuario": "black eskater", "puntaje": 4}
+        ]
+      },
+      {
+        "categoria": "menciones",
+        "usuarios": [
+          {"usuario": "Servidor", "puntaje": 1197},
+          {"usuario": "meshogang", "puntaje": 261},
+          {"usuario": "renacido", "puntaje": 85},
+          {"usuario": "Isra", "puntaje": 24},
+          {"usuario": "subaru legacy", "puntaje": 224},
+          {"usuario": "caballerito", "puntaje": 66},
+          {"usuario": "RoyManky", "puntaje": 109},
+          {"usuario": "andale elian vamos a tatuarnos", "puntaje": 25},
+          {"usuario": "milanwiche", "puntaje": 75},
+          {"usuario": "Albyst", "puntaje": 28},
+          {"usuario": "Kurco", "puntaje": null},
+          {"usuario": "tilin", "puntaje": 72},
+          {"usuario": "Poyotota", "puntaje": 34},
+          {"usuario": "chavez smith", "puntaje": 95},
+          {"usuario": "el idolo", "puntaje": 23},
+          {"usuario": "ch4", "puntaje": 70},
+          {"usuario": "black eskater", "puntaje": 6}
+        ]
+      }
+    ]
+  }
+  
